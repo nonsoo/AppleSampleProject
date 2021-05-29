e43009d06ca2a6a698d1f43b68e86e631eb1e531
Nonso <nonsootoh@gmail.com>
2021-05-29 14:55:23 -0400

    Initial Commit

---
+++ AppleSampleProject/AppleSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSampleProjectApp.swift
+//  AppleSampleProject
+//
+//  Created by Nonso Otoh on 2021-05-29.
+//
+
+import SwiftUI
+
+@main
+struct AppleSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleSampleProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleSampleProject
+//
+//  Created by Nonso Otoh on 2021-05-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
